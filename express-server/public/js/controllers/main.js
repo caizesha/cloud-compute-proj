48eee979115d94f3fddd041a77203ebcc13a5c49
JHSeng <zxc98567@gmail.com>
2019-05-30 00:55:15 +0800

    renew main.js

--- express-server/public/js/controllers/main.js
+++ express-server/public/js/controllers/main.js
@@ -3,42 +3,31 @@ const app = angular.module('bankController', []);
 // inject the Todo service factory into our controller
 app.controller('mainController', ['$scope', '$http', 'Accounts', ($scope, $http, Accounts) => {
   $scope.formData = {};
-  $scope.loading = false;
+  $scope.loading = true;
   // 定义了点击登录按钮的动作
   $scope.login = () => {
-    if ($scope.user.account == undefined) {
-      alert('用户名不能为空！');
-      return;
+    if ($scope.user.account != undefined && $scope.user.passwd != undefined) {
+      // 不知道为什么alert没用
+      alert('注册成功！');
+      console.log($scope.user.account);
+      console.log($scope.user.passwd);
+      $scope.loading = true;
+      Accounts.create($scope.user)
+          .success((data) => {
+            $scope.loading = false;
+            $scope.formData = {};
+          });
     }
-    if ($scope.user.passwd == undefined) {
-      alert('密码不能为空！');
-      return;
-    }
-    alert('注册成功！');
-    console.log($scope.user.account);
-    console.log($scope.user.passwd);
-    $scope.loading = true;
-    Accounts.create($scope.user)
-        .success((data) => {
-          $scope.loading = false;
-          $scope.formData = {};
-        });
   };
   // 定义了点击注册按钮的动作
   $scope.register = function() {
-    if ($scope.user.account == undefined) {
-      alert('用户名不能为空！');
-      return;
+    if ($scope.user.account != undefined && $scope.user.passwd != undefined && $scope.user.name != undefined) {
+      $scope.loading = true;
+      Accounts.create($scope.user)
+          .success((data) => {
+            $scope.loading = false;
+            $scope.user = {};
+          });
     }
-    if ($scope.user.passwd == undefined) {
-      alert('密码不能为空！');
-      return;
-    }
-    $scope.loading = true;
-    Accounts.create($scope.user)
-        .success((data) => {
-          $scope.loading = false;
-          $scope.user = {};
-        });
   };
 }]);
