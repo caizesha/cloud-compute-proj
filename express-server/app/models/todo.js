ee6323998d9f8b86a5fc62effc84ccbe25f15dd1
JHSeng <zxc98567@gmail.com>
2019-05-29 18:42:37 +0800

    init project

---
+++ express-server/app/models/todo.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+
+// Define the schema
+module.exports = mongoose.model('Todo', {
+    text: {
+        type: String,
+        default: ''
+    },
+    name: {
+        type:String,
+        default:""
+    },
+    value: {
+        type: String,
+        default: ''
+    },
+    account:{
+        type:String,
+        default:""
+    },
+    password:{
+        type:String,
+        default:""
+    }
+});
