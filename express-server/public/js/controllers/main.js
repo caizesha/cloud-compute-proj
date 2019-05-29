fd86790d6ff6e1ca2fe4e4bbbd1b04a5ac9a4b6f
JHSeng <zxc98567@gmail.com>
2019-05-29 23:25:44 +0800

    renew main.js

--- express-server/public/js/controllers/main.js
+++ express-server/public/js/controllers/main.js
@@ -38,16 +38,30 @@ app.controller('mainController', ['$scope', '$http', 'Todos', function($scope, $
   $scope.register = function() {
     // validate the formData to make sure that something is there
     // if form is empty, nothing will happen
-    if ($scope.user.account != undefined) {
-      $scope.loading = true;
-      // call the create function from our service (returns a promise object)
-      Todos.create($scope.user)
-      // if successful creation, call our get function to get all the new todos
-          .success(function(data) {
-            $scope.loading = false;
-            $scope.user = {}; // clear the form so our user is ready to enter another
-            // $scope.todos = data; // assign our new list of todos
-          });
+    // if ($scope.user.account != undefined) {
+    //   $scope.loading = true;
+    //   // call the create function from our service (returns a promise object)
+    //   Todos.create($scope.user)
+    //   // if successful creation, call our get function to get all the new todos
+    //       .success(function(data) {
+    //         $scope.loading = false;
+    //         $scope.user = {}; // clear the form so our user is ready to enter another
+    //         // $scope.todos = data; // assign our new list of todos
+    //       });
+    // }
+    if ($scope.user.account == undefined) {
+      alert('用户名不能为空！');
+      return;
     }
+    if ($scope.user.passwd == undefined) {
+      alert('密码不能为空！');
+      return;
+    }
+    $scope.loading = true;
+    Accounts.create($scope.user)
+        .success((data) => {
+          $scope.loading = false;
+          $scope.user = {};
+        });
   };
 }]);
