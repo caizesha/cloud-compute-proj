79fb7e648a45184cdb2d454502bacb88d39bf978
JHSeng <zxc98567@gmail.com>
2019-05-30 11:11:51 +0800

    renew main.js

--- express-server/public/js/controllers/main.js
+++ express-server/public/js/controllers/main.js
@@ -8,26 +8,21 @@ app.controller('mainController', ['$scope', '$http', 'Accounts', ($scope, $http,
   // 定义了点击登录按钮的动作
   $scope.login = () => {
     if ($scope.user.account != undefined && $scope.user.passwd != undefined) {
-      // 不知道为什么alert没用, console.log也没用
-      swal('成功', '注册账号成功', 'success');
+      swal('成功', '登录账号成功', 'success');
       console.log($scope.user.account);
       console.log($scope.user.passwd);
-      $scope.loading = true;
       Accounts.create($scope.user)
           .success((data) => {
-            $scope.loading = false;
-          // $scope.formData = {};
+            $scope.user = {};
           });
     }
   };
   // 定义了点击注册按钮的动作
   $scope.register = function() {
     if ($scope.user.account != undefined && $scope.user.passwd != undefined && $scope.user.name != undefined) {
-      $scope.loading = true;
       Accounts.create($scope.user)
           .success((data) => {
-            $scope.loading = false;
-          // $scope.user = {};
+            $scope.formData = {};
           });
     }
   };
